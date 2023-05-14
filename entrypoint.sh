b123295f2a46736d2f01ba29a530940a3157681a
gxhome77 <106860233+gxhome77@users.noreply.github.com>
2023-05-14 21:26:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='c8d890ec-66e7-463a-dcb1-4e4048fe9818'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+SS_WSPATH='/vr'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
